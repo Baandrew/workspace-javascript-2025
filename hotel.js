5614775b06b335363ce6be34f20391697e8d8b2f
Baandrew <bolandandrew242@gmail.com>
2025-11-10 13:13:23 -0500

    commit

--- hotel.js
+++ hotel.js
@@ -12,14 +12,31 @@ var guest = {
 function reservation() {
 // get  user reservation info
 var guest = {
-    name: document.getElementById("guestname").value,
+    guestname: document.getElementById("guestname").value,
     checkin : document.getElementById("checkin").value,
+    guestgroup : document.getElementById("guestgroup").value,
     roomtype : document.getElementById("room").value,
 };
 guestgroup.push(guest);
 consolwe.log(guestgroup);
 }
+// show reservation
+function showbookings() { 
+    // show the whole guestgroup
+    var message= "";
+    for(var i in guestgroup) {
+        var guest = guestgroup[i];
+        message += "<li>" + guest.guestname + " " + guest.checkin + " " + guest.roomtype + "</li>";
+    }
+    // display through alert or display on the page
+    document.getElementById("listofbooking").innerHTML = message;
 
+}
 
 //register event
 document.getElementById("register").addEventListener("click", reservation);
+// put event listner
+document.getElementById("lists").addEventListener("click", listbooking);
+// click the list reservation button to show bookings
+function listbooking() {
+    var list = "<h2>Current Reservations</h2><ul>";}
